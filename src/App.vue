6358bcaef66f715e607f2303d51b50418066700e
cat_glb <goodCat_glb@eatfish.com>
2021-01-25 15:27:52 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>黑马头条管理系统练习</h1>
+    <!-- 路由入口 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+
+</style>
